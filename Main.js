886dfdc93a206b9afce8a09c6f9307f3b5627834
SirriRyisa <sirriryisa@gmail.com>
2021-12-24 15:05:53 +0100

    Fixed linters

--- Main.js
+++ Main.js
@@ -8,13 +8,13 @@ document.querySelector('.toggleclose').addEventListener('click', () => {
 
 document.querySelectorAll('.mobile-menu a').forEach((element) => {
   element.addEventListener('click', () => {
-  document.querySelector('.hamburger').style.display = 'none';
+    document.querySelector('.hamburger').style.display = 'none';
   });
 });
 
 document.querySelectorAll('.toggle').forEach((link) => link.addEventListener('click', () => {
   document.querySelector('.toggle').style.display = 'none';
-}))
+}));
 
 const form = document.getElementByclassName('dataForm');
 const storeName = form.elements.name;
